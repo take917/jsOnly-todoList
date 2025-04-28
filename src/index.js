be75c8081aefebed49294c452294009d5664e599
zero <1tktk2tk4@gmail.com>
2025-04-28 19:26:07 +0900

    make addTask

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-var val = "ssss";
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  alert(inputText);
 
-console.log(val);
+  // テキストBOX入力値を初期化
+  document.getElementById("add-text").value = "";
+
+  // liタグの作成
+  const li = document.createElement("li");
+
+  // divタグの作成
+  const div = document.createElement("div");
+  //   生成divにクラスを適用
+  div.className = "list-row";
+
+  //  Pタグ作成
+  const p = document.createElement("p");
+  // 生成divにクラスを適用
+  p.className = "todo-item";
+
+  // 生成したタグにテキストを入れる
+  p.innerText = inputText;
+  div.appendChild(p);
+  li.appendChild(div);
+  console.log(li);
+  console.log(document.getElementById("incomplete-list"));
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document.getElementById("add-button").addEventListener("click", onClickAdd);

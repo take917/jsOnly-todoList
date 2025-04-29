4191d95adf54d9668fc75d02fe6559f8823b37fe
zero <1tktk2tk4@gmail.com>
2025-04-30 07:53:56 +0900

    merge

--- src/index.js
+++ src/index.js
@@ -7,14 +7,13 @@ const onClickAdd = () => {
 };
 
 // 未完了のTODOを作成
-
 const createIncompleteTodo = (todo) => {
   // liタグの作成
   const li = document.createElement("li");
 
   // divタグの作成
   const div = document.createElement("div");
-  //   生成divにクラスを適用
+  //   生成したdivにクラスを適用
   div.className = "list-row";
 
   //  Pタグ作成
@@ -22,20 +21,26 @@ const createIncompleteTodo = (todo) => {
 
   // 完了ボタン作成
   const completeButton = document.createElement("button");
+  //ボタンの表示を完了にする
   completeButton.innerText = "完了";
+  //ボタンクリック時のイベントの設定
   completeButton.addEventListener("click", () => {
+    // 親要素の取得
     const moveTarget = completeButton.closest("li");
+    // 該当の次の要素を削除
     completeButton.nextElementSibling.remove();
+    // 要素の削除
     completeButton.remove();
     const backButton = document.createElement("button");
     backButton.innerText = "戻す";
 
     backButton.addEventListener("click", () => {
+      //   該当要素の前の要素のテキストを取得
       const todoText = backButton.previousElementSibling.innerText;
       createIncompleteTodo(todoText);
       backButton.closest("li").remove();
     });
-
+    // ターゲットの最初の要素に要素を追加
     moveTarget.firstElementChild.appendChild(backButton);
     document.getElementById("complete-list").appendChild(moveTarget);
   });
